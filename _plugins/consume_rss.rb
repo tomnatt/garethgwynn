5888a7137b27cb228d0c96acf53125aaa7417c8d
tomnatt <github@tomnatt.com>
2013-10-31 18:55:01 +0000

    first pass at pulling an RSS feed and putting on a page

--- _plugins/consume_rss.rb
+++ _plugins/consume_rss.rb
@@ -1,12 +1,29 @@
 class ConsumeRss < Liquid::Tag
- 
+
+    require 'simple-rss'
+    require 'net/http'
+    require 'cgi'
+
     def initialize(tag_name, text, tokens)
         super
-        @text = text
+
+        # get the feed
+        url = URI.parse(text)
+        req = Net::HTTP::Get.new(url.path)
+        res = Net::HTTP.get(url) {|http|
+            http.request(req)
+        }
+
+        # get the first item in a useful format
+        @feed = SimpleRSS.parse(res).items
+
     end
  
     def render(context)
-        "RSS feed here - some text: #{@text}"
+        "<h2>#{@feed.first.title}</h2>
+        <p>#{CGI.unescapeHTML(@feed.first.content)}</p>
+        <h2>#{@feed[1].title}</h2>
+        <p>#{CGI.unescapeHTML(@feed[1].content)}</p>"
     end
  
     Liquid::Template.register_tag "rss", self
